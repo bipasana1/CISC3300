6e448ad8cb8fd1c4c9216561092431849e7dabe2
bipasana1 <143720787+bipasana1@users.noreply.github.com>
2024-02-19 17:50:04 -0500

    Add files via upload

---
+++ Homework 4/cat.js
@@ -0,0 +1,72 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+]
+
+function getAvailableCats(catsArray) {
+    return catsArray.filter (cats => cats.adoptionStatus === 'available').map(cats => cats.name)
+
+};
+
+function generateAdoptionSentence(availableCats) {
+    return `Congratulations on adopting ${availableCats.join(', ')}! They're now part of your family.`;
+}
+
+
+const availableCats= getAvailableCats(cats);
+const sentence = generateAdoptionSentence(availableCats);
+console.log(sentence);
